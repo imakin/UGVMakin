31a0d73ec517be5670e4c8f23c13bca02f49db1a
muhammad izzulmakin <makin@brambleco.com>
2020-02-24 09:20:52 +0700

    add car4

--- car4/car/apprelay.lua
+++ car4/car/apprelay.lua
@@ -89,6 +89,41 @@ function net_init_udp()
   end)
 end
 
+
+function net_init_http()
+  srv = net.createServer(net.TCP)
+  print("listening to http")
+  srv:listen(80, function(server)
+    server:on("receive", function(sck, payload)
+      --~ print(payload)
+      --~ print("slash n in:")
+      --~ print(string.find(payload,' HTTP')) --http packet first line is: GET /path-url HTTP/1.1
+      sck:send("HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n<html> <head> <style> button { font-size: 100px; font-family: 'Oxygen Mono', Arial; padding: 40px; } </style> </head> <body> <script> var p = 1359; var steer = 84; var drive = 0b1111; function send() { var newp = (steer<<4) + drive; if (newp!=p) { p = newp; document.location.pathname = '/'+p; } } function l(){ steer = 84-15; send(); } function r(){ steer = 84+15; send(); } function f(){ drive = 2; send(); } function b(){ drive = 1; send(); } </script> <div> <button onclick='l()'>&lt;</button> <button onclick='r()'>&gt;</button> <div> <div> <button onclick='f()'>F</button> <button onclick='b()'>B</button> <div> </body> </html>")
+      
+      local datanum = tonumber(payload:match("%d+"))
+      if datanum==nil then
+        --~ print("tonumber(message) results in nil")
+        return false
+      end
+      
+      gpio.write(driverIN1, (bit.isset(datanum, 0) and 1 or 0))
+      gpio.write(driverIN2, (bit.isset(datanum, 1) and 1 or 0))
+      gpio.write(driverIN3, (bit.isset(datanum, 2) and 1 or 0))
+      gpio.write(driverIN4, (bit.isset(datanum, 3) and 1 or 0))
+      
+      datanum = bit.rshift(datanum, 4)
+      print('steer to')
+      print(datanum)
+      car_steer(datanum)
+      return true
+      
+    end)
+    server:on("sent", function(sck) sck:close() end)
+  end)
+end
+_G.net_init_http = net_init_http
+
+
 function remote_init_as_client()
   gpio.write(led, 1)
   wifi.setmode(wifi.STATION)
@@ -124,7 +159,7 @@ function remote_init_as_server()
     gpio.write(led, 0)
     print("connected: ")
     print(mac)
-    net_init_udp()
+    net_init_http()
    end
   ssid.stadisconnected_cb = function(mac, aid)
     print("disconnected: ")
